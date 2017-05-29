d7e96a4f898a124d4db808a48f17846a620209e4
Premysl Kafka <premysl.kafka@hpe.com>
2017-05-29 18:49:28 +0200

    MongoConnectorFactory also checks connection to mongo

--- src/main/java/org/kafkapre/sentence/generator/persistence/impl/MongoConnectorFactory.java
+++ src/main/java/org/kafkapre/sentence/generator/persistence/impl/MongoConnectorFactory.java
@@ -2,9 +2,14 @@ package org.kafkapre.sentence.generator.persistence.impl;
 
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kafkapre.sentence.generator.AppConfiguration;
 
 public class MongoConnectorFactory {
+
+    private static final Logger logger = LogManager.getLogger(MongoWordDAL.class);
 
     private final AppConfiguration conf;
 
@@ -12,13 +17,17 @@ public class MongoConnectorFactory {
         this.conf = conf;
     }
 
-    public MongoClient create() {
-        MongoClient mongoClient = new MongoClient(conf.getMongoHost(), conf.getMongoPort());
-
-        // TODO check ping
-
+    public MongoClient create() throws InterruptedException {
+        MongoClient mongoClient;
+        try {
+            mongoClient = new MongoClient(conf.getMongoHost(), conf.getMongoPort());
+            logger.info("Connected to MongoDB OK. [host:{}, port:{}]", conf.getMongoHost(), conf.getMongoPort());
+        }catch (MongoException ex){
+            String msg = String.format("Cannot connect to MongoDB [host:%s, port:%d]",
+                    conf.getMongoHost(), conf.getMongoPort());
+            throw new RuntimeException(msg, ex);
+        }
         return mongoClient;
     }
-
 
 }
