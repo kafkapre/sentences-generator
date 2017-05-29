e650effed99745ea96865cf3180cff163c07a78f
Premysl Kafka <premysl.kafka@hpe.com>
2017-05-29 23:55:12 +0200

    Java doc added

--- src/main/java/org/kafkapre/sentence/generator/persistence/api/SentenceDAL.java
+++ src/main/java/org/kafkapre/sentence/generator/persistence/api/SentenceDAL.java
@@ -11,18 +11,61 @@ import java.util.Optional;
 
 public interface SentenceDAL {
 
+    /**
+     * This method generates and stores sentence from given words
+     * @param words Sentence will be generated from given words.
+     * @exception PersistenceException On persistence error.
+     * @return Sentence Returns generated sentence.
+     */
     Sentence createAndStoreSentence(Words words);
 
+    /**
+     * This method increments "showDisplayCount" property of
+     * sentence determined by id.
+     * @param id Sentence id.
+     * @exception PersistenceException On persistence error.
+     * @return boolean Returns if operation was successful.
+     */
     boolean incrementSentenceShowDisplayCount(ObjectId id);
 
+    /**
+     * This method increments "sameGeneratedCount" property of
+     * sentence determined by id.
+     * @param id Sentence id.
+     * @exception PersistenceException On persistence error.
+     * @return boolean Returns if operation was successful.
+     */
     boolean incrementSentenceSameGeneratedCount(ObjectId id);
 
+    /**
+     * This method returns sentence with particular id.
+     * @param id Sentence id.
+     * @exception PersistenceException On persistence error.
+     * @return Optional<Sentence> Returns Optional.empty() if not found or Optional.of({sentence}) otherwise.
+     */
     Optional<Sentence> getSentence(ObjectId id);
 
+    /**
+     * This method returns list of sentences with particular hash
+     * @param hash Sentence's hash of words.
+     * @exception PersistenceException On persistence error.
+     * @return List<Sentence> Returns list of sentences with particular hash
+     */
     List<Sentence> getSentences(int hash);
 
+    /**
+     * This method returns list of sentences with particular words.
+     * @param words Sentence's words.
+     * @exception PersistenceException On persistence error.
+     * @return List<Sentence> Returns list of sentences with particular words
+     */
     List<Sentence> getSentences(Words words);
 
+    /**
+     * This method returns list of all sentences in basic form.
+     * @exception PersistenceException On persistence error.
+     * @return List<BaseSentence> Returns list of all sentences in basic form.
+     */
     List<BaseSentence> getAllBaseSentences();
 
 }
--- src/main/java/org/kafkapre/sentence/generator/persistence/api/WordDAL.java
+++ src/main/java/org/kafkapre/sentence/generator/persistence/api/WordDAL.java
@@ -8,12 +8,37 @@ import java.util.Optional;
 
 public interface WordDAL {
 
+    /**
+     * This method inserts given word if it has not been already inserted,
+     * otherwise updates particular word.
+     * @param word
+     * @exception PersistenceException On persistence error.
+     */
     void putWord(Word word);
 
+    /**
+     * This method returns word with particular id.
+     * @param id Word id.
+     * @exception PersistenceException On persistence error.
+     * @return Optional<Word> Returns Optional.empty() if not found or Optional.of({word}) otherwise.
+     */
     Optional<Word> getWord(String id);
 
+
+    /**
+     * This method returns word with particular category randomly.
+     * @param category Word category.
+     * @exception PersistenceException On persistence error.
+     * @return Optional<Word> Returns Optional.empty() if any word with particular category
+     * does not exist or Optional.of({word}) otherwise.
+     */
     Optional<Word> getRandomWord(WordCategory category);
 
+    /**
+     * This method returns list of all words in basic form.
+     * @exception PersistenceException On persistence error.
+     * @return List<Word> Returns list of all words in basic form.
+     */
     List<Word> getAllWords();
 
 }
